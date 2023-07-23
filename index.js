7e6d5f3f42db83206789207ea3f6fafaeb313719
Vivek Patel <vpatel360@gmail.com>
2023-07-22 21:50:50 -0400

    commit tree e006ba1c0e3a394e7b8d2fb026e73387b8526bec

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1, string.length)
+
+  }
+  static sanitize(string){
+    return string.replace(/[^a-z0-9\.,_' -]/gi,"");
+  }
+  static titleize(string){
+    const arr = string.split(" ").map(word => {
+      if (['the','a','an','but','of','and','for','at','by','from'].includes(word)){
+        return word
+      }
+      else {
+        return this.capitalize(word)
+      }
+     })
+     return this.capitalize(arr.join(' '))
+  }
+} 
